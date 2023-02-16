d629ee09544ce057dcc7a49326f201332ec7348e
munenecarl <munenecarl@gmail.com>
2023-02-16 20:04:45 +0300

    addition of C files

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Descreption: A C program that print with printf function
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
