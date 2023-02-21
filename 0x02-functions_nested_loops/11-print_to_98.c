1b929c4f2f0bcf4f49660ad44f943cbd075daffb
munenecarl <munenecarl@gmail.com>
2023-02-22 01:21:10 +0300

    corrections

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,16 +11,29 @@ void print_to_98(int n)
 	if (n < 98)
 	{
 		for (; n <= 98; n++){
-			printf("%d, ",n);
+			if (n != 98)
+			{
+				printf("%d, ",n);
+			}
+			else
+			{
+				printf("%d",n);
+			}	
 		}
 	}
 	else if (n > 98)
 	{
 		for (; n >= 98; n--)
 		{
-			printf("%d, ",n);
+			if (n != 98)
+			{
+				printf("%d, ",n);
+			}
+			else
+			{
+				printf("%d",n);
+			}
 		}
-
 	}
 	else
 	{
