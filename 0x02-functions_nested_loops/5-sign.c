50f8fd097e4fb72c346895cce74f4001116d7fe0
munenecarl <munenecarl@gmail.com>
2023-02-22 01:39:45 +0300

    correction

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(43 + 0);
+		putchar(43 + 0);
 		return (+1);
 	}
 	else if (n == 0)
 	{
-		_putchar(0 + 48);
+		putchar(0 + 48);
 		return (0);
 	}
 	else
 	{
-		_putchar(45 + 0);
+		putchar(45 + 0);
 		return (-1);
 	}
 }
