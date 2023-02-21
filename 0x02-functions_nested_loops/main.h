c1a7aa2db8c04d0ba7ce23dc1ac3ef036395b196
munenecarl <munenecarl@gmail.com>
2023-02-21 20:37:06 +0300

    addition of main_h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+ * main_h contains definition of all functions in this folder
+ *
+ * Return: void no return type
+*/
+
+void print_alphabet(void);
+int _putchar(char c);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+
+#endif
