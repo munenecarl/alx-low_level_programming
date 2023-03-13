eb5ff64f4ac97c2c840dcb5839f5c513b8939654
munenecarl <munenecarl@gmail.com>
2023-03-13 19:32:36 +0300

    corrections

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,28 +3,63 @@
 #include <ctype.h>
 
 /**
+ * isnumber - to find out if number is a digit
+ *
+ * @number: the input to be tested
+ *
+ * Return: 1 if not number, 0 if number
+*/
+
+int isnumber(char number[])
+{
+	int i, len;
+
+	len = strlen(number);
+
+	for (i = 0; i < len; i++)
+	{
+		if (!isdigit(number[i]))
+		{
+			return (1);
+		}
+		return (0);
+	}
+
+}
+
+/**
  * main - entry point of the program
- * 
+ *
+ * @argc: number of arguments
+ * @argv: array containing arguments
+ *
  * Return: 1 if successful or 0 if failure
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-	int num1, num2, result;
-	
+	int i, num1, num2, result;
+
 	if (argc == 1)
 	{
-		printf("0\n"); 
+		printf("0\n");
 	}
-
-	if (sscanf(argv[1], "%d", &num1) != 1 || sscanf(argv[2], "%d", &num2 != 1))
+	else
 	{
-		printf("Error\n");
-		return (1);
+		for (i = 1; i < argc - 1; i++)
+		{
+			if (isnumber(argv[i]) == 0)
+			{
+				result += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", result);
 	}
-
-	result = num1 + num2;
-	printf("%d\n");
 
 	return (0);
 }
