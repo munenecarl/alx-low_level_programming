c7d43925c96f90110667403b12e4d708d2426631
munenecarl <munenecarl@gmail.com>
2023-03-13 18:43:19 +0300

    corrections

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - entry point of the program
@@ -11,25 +12,19 @@
 
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-	
+	int result;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	if (sscanf(argv[1], "%d", &num1) != 1 || sscanf(argv[2], "%d", &num2 != 1))
+	else
 	{
-		printf("Error\n");
-		return (1);
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
 	}
 
-	int result;
-
-	result = num1 * num2;
-	printf("%d\n", result);
-
 	return (0);
-
 }
