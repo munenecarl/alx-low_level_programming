985302cfbeb2a1bbbe60273f2189e49c1ed97a55
munenecarl <munenecarl@gmail.com>
2023-03-13 20:05:07 +0300

    corrections

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -17,6 +17,7 @@ int main(int argc, char **argv)
 	if (argc != 2)
 	{
 		printf("Error\n");
+		return (1);
 	}
 
 	cents = atoi(argv[1]);
@@ -27,11 +28,15 @@ int main(int argc, char **argv)
 		return (0);
 	}
 	num_coins = 0;
-	num_coins += cents / 25; cents %= 25;
-    	num_coins += cents / 10; cents %= 10;
-    	num_coins += cents / 5; cents %= 5;
-    	num_coins += cents / 2; cents %= 2;
-    	num_coins += cents;
+	num_coins += cents / 25;
+	cents %= 25;
+	num_coins += cents / 10;
+	cents %= 10;
+	num_coins += cents / 5;
+	cents %= 5;
+	num_coins += cents / 2;
+	cents %= 2;
+	num_coins += cents;
 	printf("%d\n", num_coins);
 
 	return (0);
