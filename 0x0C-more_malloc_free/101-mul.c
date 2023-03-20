d1da161f554fa94c0c47b5a8f5fa802122046558
munenecarl <munenecarl@gmail.com>
2023-03-20 14:47:08 +0300

    Addition of Hfiles

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,18 @@
 #include "main.h"
 
+
+
+
 /**
  * main - program entry point
  * 
+ * @num1: first number
+ * @num2: second number
  * 
+ * Return value if program runs successfully
 */
+
+int main()
+{
+
+}
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Return: 0 if successful, 1 if failed
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
