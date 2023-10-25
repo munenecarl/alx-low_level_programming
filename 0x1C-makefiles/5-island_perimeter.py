6a813d488dda5fb2b2bd29b6957ef24ca7a6c0b1
munenecarl <munenecarl@gmail.com>
2023-10-25 19:07:46 +0300

    pycodestyle conform

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -16,5 +16,4 @@ def island_perimeter(grid):
                     perimeter += 1
                 if j == len(grid[i]) - 1 or grid[i][j+1] == 0:
                     perimeter += 1
-
     return perimeter
